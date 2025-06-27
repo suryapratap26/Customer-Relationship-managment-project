248343793d355e633f721496f1917c242cd7e7c7
suryapratap26 <suryapratapsinghcundawat26@gmail.com>
2025-06-27 23:30:44 +0530

    Customer RelationShip Project

---
+++ src/main/java/com/CustomerRelationshipManagement/dto/PasswordResetRequest.java
@@ -0,0 +1,14 @@
+package com.CustomerRelationshipManagement.dto;
+
+
+public class PasswordResetRequest {
+    private String newPassword;
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
